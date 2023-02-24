acbf67478ae3e1c07eba055d8e08d44f3d0e77cc
angie <amaticorena103@hotmail.com>
2023-02-23 19:35:54 -0500

    add title and additional css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
-// import * as React from "react"
-
 import React from "react";
 import "/styles/styles.css"
 
 function App () {
-  return <h1>Angie Maticorena</h1>
+  return (
+    <div id="introduction">
+      <h2 className="title">Hi! I am</h2>
+      <h1 id="name">Angie Maticorena</h1>
+      <h2 className="title">Full Stack Software Engineer with an eye for creativity</h2>
+    </div>
+  )
 }
 
 export default App;
