24aa804b2e5538c818e2707dbd68078fa1b79066
angie <amaticorena103@hotmail.com>
2023-03-01 20:23:21 -0500

    styled navbar, background, changed font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,15 +4,19 @@ import "/styles/styles.css";
 
 function Layout ( {children}) {
   return (
-    <div id="introduction">
-      <h2 className="title">Hi! I am</h2>
-      <h1 id="name">Angie Maticorena</h1>
-      <h2 className="title">Full Stack Software Engineer with an eye for creativity</h2>
-      <Navbar />
-        <div id="container">
-          {children}
-        </div>
-    </div>
+    <section>
+      <div id="introduction">
+        <h2 className="title">Hi! My name is</h2>
+        <h1 id="name">Angie Maticorena</h1>
+        <h2 className="title">Full Stack Software Engineer</h2>
+      </div >
+      <div id="navsec">
+         <Navbar />
+         <div id="container">
+             {children}
+         </div>
+      </div>
+    </section>
   );
 };
 
