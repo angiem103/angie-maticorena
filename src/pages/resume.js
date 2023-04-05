5187c158209c7a9eec2c03211d3cf3f5f826801c
angie <amaticorena103@hotmail.com>
2023-04-04 21:28:52 -0400

    fix resume issue

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,24 +1,20 @@
 import React from "react";
-import PDF from "../images/resume.pdf";
-import packageJson from "/package.json"
+import Pdf from "../images/resume.pdf";
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 function Resume(){ 
 
-    
-const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
-console.log(pdfjsVersion)
 
     return (
         <div className="resume" >
             <h1>Resume</h1>   
                 <div className="pdf-container" >
-                <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
-                        <Viewer fileUrl={PDF} />  
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                        <Viewer fileUrl={Pdf} />  
                     </Worker>       
                 </div>
-            <a href={PDF} type="button" id="resume-btn" target="_blank"  rel="noreferrer">View Resume</a> 
+            <a href={Pdf} type="button" id="resume-btn" target="_blank"  rel="noreferrer">View Resume</a> 
         </div>
     )
 };
