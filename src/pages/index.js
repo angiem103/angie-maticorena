d8375711f3b40c3ff143a3c1f07c5b3c408e0d11
angie <amaticorena103@hotmail.com>
2023-04-04 22:26:19 -0400

    add about me section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 import "/styles/styles.css";
 
 function App () {
   return (
 
       <div className="aboutme">
-        <h3>Hi, my name is Angie. I live in Union City, New Jersey. I recently graduated Flatiron School with a certification in Software Engineering. During my time in school I learned HTML, CSS, REACT.JS, RUBY, RUBY ON RAILS</h3>
+        <h3 id="about-me">Hi, my name is Angie :) . I live in Union City, New Jersey. I recently graduated Flatiron School with a certification in Software Engineering. During my time at school, I learned HTML, CSS, REACT.JS, RUBY, and RUBY ON RAILS. I am looking for my next career opportunity while expanding my programming knowledge. I like to spend my free time with family and friends, learning a new creative hobby, or trying new foods/experiences. Please feel free to check out my resume or contact me for more information, or just to chat.</h3>
+        <StaticImage src="../images/angie.png" placeholder="blurred" id="angie"/>
       </div>
 
   );
