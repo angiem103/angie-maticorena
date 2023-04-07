110fa7c697ed1293a03292818f9e830299f65c09
angie <amaticorena103@hotmail.com>
2023-04-07 18:40:10 -0400

    deleted template folder, updated image on about me

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ function App () {
 
       <div className="aboutme">
         <h3 id="about-me">Hi, my name is Angie :) . I live in Union City, New Jersey. I recently graduated Flatiron School with a certification in Software Engineering. During my time at school, I learned HTML, CSS, REACT.JS, RUBY, and RUBY ON RAILS. I am looking for my next career opportunity while expanding my programming knowledge. I like to spend my free time with family and friends, learning a new creative hobby, or trying new foods/experiences. Please feel free to check out my resume or contact me for more information, or just to chat.</h3>
-        <StaticImage src="../images/angie.png" placeholder="blurred" id="angie"/>
+        <StaticImage src="../images/angie.png" placeholder="blurred" className="angie"/>
       </div>
 
 
