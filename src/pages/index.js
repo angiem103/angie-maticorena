d30a74a41824a32a1319c04959f656fa0dac2905
angie <amaticorena103@hotmail.com>
2023-05-25 22:07:16 -0400

    update paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ function App () {
   return (
 
       <div className="aboutme">
-        <h3 id="about-me">Hi, my name is Angie :) . I live in Union City, New Jersey. I recently graduated Flatiron School with a certification in Software Engineering. During my time at school, I learned HTML, CSS, REACT.JS, RUBY, and RUBY ON RAILS. I am looking for my next career opportunity while expanding my programming knowledge. I like to spend my free time with family and friends, learning a new creative hobby, or trying new foods/experiences. Please feel free to check out my resume or contact me for more information, or just to chat.</h3>
+        <h3 id="about-me">Hi, my name is Angie :) . I live in Union City, New Jersey. I recently graduated Flatiron School with a certification in Software Engineering. During my time at school, I learned HTML, CSS, REACT.JS, RUBY, and RUBY ON RAILS. I am looking for my next career opportunity while learning Python. I like to spend my free time with family and friends, learning a new creative hobby, or trying new foods/experiences. Please feel free to check out my resume or contact me for more information, or just to chat.</h3>
         <StaticImage src="../images/angie.png" width={400} height={500} placeholder="blurred" className="angie"  />
       </div>
 
